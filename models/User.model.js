048b62507e540caad76a20476b440ca6a8aea822
trinigelos <trinidadgelos7@gmail.com>
2024-11-05 18:12:02 +0100

    first commit

---
+++ models/User.model.js
@@ -0,0 +1,33 @@
+const { Schema, model } = require("mongoose");
+
+// TODO: Please make sure you edit the user model to whatever makes sense in this case
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true, 
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true, 
+      trim: true,
+      match: [/.+@.+\..+/, "Please enter a valid email address"],
+    }
+  },
+  {
+    // this second object adds extra properties: `createdAt` and `updatedAt`
+    timestamps: true,
+  }
+);
+
+const User = model("User", userSchema);
+
+module.exports = User;
